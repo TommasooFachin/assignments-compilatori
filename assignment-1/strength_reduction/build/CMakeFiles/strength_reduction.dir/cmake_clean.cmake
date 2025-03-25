a75c469cb18d18c5614354b4a3d5d9d3368779cb
aimenaimenaimen <aimen.omari02@gmail.com>
2025-03-25 11:23:08 +0100

    ciao!

---
+++ assignment-1/strength_reduction/build/CMakeFiles/strength_reduction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/strength_reduction.dir/strength_reduction.cpp.o"
+  "CMakeFiles/strength_reduction.dir/strength_reduction.cpp.o.d"
+  "libstrength_reduction.dylib"
+  "libstrength_reduction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/strength_reduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assignment-1/strength_reduction/build/CMakeFiles/strength_reduction.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for strength_reduction.
+# This may be replaced when dependencies are built.
